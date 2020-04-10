66cbb17273650e6c0113ef3274481099c4c7e72f
Yannik <36708997+Alphaprot@users.noreply.github.com>
2020-04-10 19:19:03 +0200

    Add files via upload

--- Lueftung.ino
+++ Lueftung.ino
@@ -1,6 +1,6 @@
 // pin number, where relay is connected
-#define RELAY1_PIN 9 //Stufe 1
-#define RELAY2_PIN 10 //Stufe 3
+#define RELAY1_PIN 9 //Stufe 3
+#define RELAY2_PIN 10 //Stufe 1
 #define RELAY3_PIN 11 //Schalter L
 #define RELAY4_PIN 12 //Schalter N
 // variable to store current relay state
@@ -9,10 +9,11 @@ byte stufe2 = 1;
 byte stufe3 = 0;
 byte powerState = 1;
 
-ZUNO_SETUP_CHANNELS(ZUNO_SWITCH_BINARY(getStufe1State, setStufe1State),
-                    ZUNO_SWITCH_BINARY(getStufe3State, setStufe3State),
-                    ZUNO_SWITCH_BINARY(getPowerState, setPowerState),
-                    ZUNO_SENSOR_BINARY(ZUNO_SENSOR_BINARY_TYPE_GENERAL_PURPOSE, getStufe2State));
+ZUNO_SETUP_CHANNELS(ZUNO_SWITCH_BINARY(getPowerState, setPowerState),
+                    ZUNO_SWITCH_BINARY(getStufe1State, setStufe1State),
+                    ZUNO_SWITCH_BINARY(getStufe3State, setStufe3State)
+                    //ZUNO_SENSOR_BINARY(General, getStufe2State),
+                   );
 
 
 void setup() {
@@ -22,6 +23,8 @@ void setup() {
   pinMode(RELAY4_PIN, OUTPUT);
   digitalWrite(RELAY1_PIN, HIGH);
   digitalWrite(RELAY2_PIN, HIGH);
+  digitalWrite(RELAY3_PIN, HIGH);
+  digitalWrite(RELAY4_PIN, HIGH);
 }
 
 void loop() {
@@ -31,9 +34,9 @@ void loop() {
 byte getStufe1State() {
   return stufe1;
 }
-byte getStufe2State() {
+/*byte getStufe2State() {
   return stufe2;
-}
+  }*/
 byte getStufe3State() {
   return stufe3;
 }
@@ -42,24 +45,41 @@ byte getPowerState() {
 }
 
 void setStufe1State(byte value) {
-  digitalWrite(RELAY2_PIN, HIGH);
-  digitalWrite(RELAY1_PIN, (value > 0) ? LOW : HIGH);
-  stufe2 = (value > 0) ? 0 : 1;
-  stufe1 = value;
-  stufe3 = 0;
+  if (powerState == 1) {
+    digitalWrite(RELAY1_PIN, HIGH);
+    digitalWrite(RELAY2_PIN, (value > 0) ? LOW : HIGH);
+    stufe2 = (value > 0) ? 0 : 1;
+    stufe1 = value;
+    stufe3 = 0;
+  }
+  else {}
+  for (int i = 1; i < 5; i++) {
+    zunoSendReport(i);
+  }
 }
 void setStufe3State(byte value) {
-  digitalWrite(RELAY1_PIN, HIGH);
-  digitalWrite(RELAY2_PIN, (value > 0) ? LOW : HIGH);
-  stufe2 = (value > 0) ? 0 : 1;
-  stufe3 = value;
-  stufe1 = 0;
+  if (powerState == 1) {
+    digitalWrite(RELAY2_PIN, HIGH);
+    digitalWrite(RELAY1_PIN, (value > 0) ? LOW : HIGH);
+    stufe2 = (value > 0) ? 0 : 1;
+    stufe3 = value;
+    stufe1 = 0;
+  }
+  else {}
+  for (int i = 1; i < 5; i++) {
+    zunoSendReport(i);
+  }
 }
 void setPowerState(byte value) {
-  digitalWrite(RELAY3_PIN, (value > 0) ? LOW : HIGH);
-  digitalWrite(RELAY4_PIN, (value > 0) ? LOW : HIGH);
+  digitalWrite(RELAY3_PIN, (value > 0) ? HIGH : LOW);
+  digitalWrite(RELAY4_PIN, (value > 0) ? HIGH : LOW);
   digitalWrite(RELAY1_PIN, HIGH);
   digitalWrite(RELAY2_PIN, HIGH);
+  stufe1 = 0;
   stufe2 = (value > 0) ? 1 : 0;
+  stufe3 = 0;
   powerState = value;
+  for (int i = 1; i<  5; i++) {
+    zunoSendReport(i);
+  }
 }
